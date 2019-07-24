e27aa1b9876313e6dbbf56059ee179bdc031cb5a
Arunkumar K <arunkk13@in.ibm.com>
2019-07-24 10:45:03 +0530

    arun

--- Assesments/Assesment2-23.07.2019/carts.js
+++ Assesments/Assesment2-23.07.2019/carts.js
@@ -1,19 +1,16 @@
 const router=require('express').Router();
 const adding=require('./productclass').adding;
-const cors = require('cors'); 
 const a1=new adding();
-server.use(cors());
-server.use(parser.json());
-server.get('/',(req,res)=>{
+router.get('/',(req,res)=>{
     res.setHeader('content-type','application/json');
     res.end(JSON.stringify({
         add1 : a1.display()
     }));
 });
-server.post('/add1',(rq,rs)=>
+router.post('/add1',(rq,rs)=>
 {
-    res.setHeader('content-type','application/json');
-  res.end(JSON.stringify(
+    rs.setHeader('content-type','application/json');
+  rs.end(JSON.stringify(
      {
          add1:a1.add3(rq.body)
      }));
--- Assesments/Assesment2-23.07.2019/prousers.js
+++ Assesments/Assesment2-23.07.2019/prousers.js
@@ -1,10 +1,7 @@
 const router=require('express').Router();
-const prod=require('./productclass').prod;
-const cors = require('cors'); 
+const prod=require('./productclass').prod; 
 const p1=new prod();
-server.use(cors());
-server.use(parser.json());
-server.get('/',(req,res)=>
+router.get('/',(req,res)=>
 {
     res.setHeader('content-type','application/json');
       res.end(JSON.stringify(
